37a5a650c3819c2a10dfd8a8a8e8d4de57d781cc
yaowuya <zrfyaowuya@126.com>
2018-02-09 14:21:41 +0800

    初步搭建项目框架

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  // 是否生产模式
+  production: true,
+  // 加载提示语
+  loadingText: '正在加载,请稍候...',
+  // 接口地址
+  server: 'http://120.25.208.138:3018/',
+  // 接口超时时间
+  timeout: 60 * 1000,
+  // sidebar  or   topnav
+  layout: 'sidebar',
+
+  pageSize: 10
+};
