d97db1caba03e1850f41035ec18eecf4c07cb5a2
yaowuya <zrfyaowuya@126.com>
2018-03-01 17:31:40 +0800

    布局

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule} from "@angular/forms";
+import {LayoutRoutingModule} from './layout-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    LayoutRoutingModule
+  ],
+  declarations: []
+})
+
+export class AuthModule{
+
+}
